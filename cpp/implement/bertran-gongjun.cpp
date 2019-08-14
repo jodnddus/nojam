988de6a8e09a4c6cf02f69ca955cdf1b15c151cb
조웅연 <lockhost23@gmail.com>
2019-08-15 03:53:44 +0900

    기초 구현문제 풀기(베르트랑 공준 #4948)

--- cpp/implement/bertran-gongjun.cpp
+++ cpp/implement/bertran-gongjun.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 
-int n,arr[123456];
+int n,su=0;
+int arr[500000];
 
 void getPrime(int num) {
-  for (int i=2; i<=num; i++) arr[i] = i;
+  for (int i=2; i<=(num*2); i++) arr[i] = i;
 
-  for (int i=2; i<=num; i++) {
+  for (int i=2; i<=(num*2); i++) {
     if (arr[i] == 0) continue;
-    for (int j=i+i; j<=num; j+=i) arr[j] = 0;
+    for (int j=i+i; j<=(num*2); j+=i) arr[j] = 0;
   }
+}
+
+int getPrimeSu(int num) {
+  su=0;
+
+  for (int i=(num+1); i<=(num*2); i++) {
+    if (arr[i] != 0) su++;
+  }
+
+  return su;
 }
 
 int main()
 {
-  do {
+  while (1) {
     std::cin >> n; 
+    if (n == 0) break;
     getPrime(n);
-  } while(n != 0)
+    std::cout << getPrimeSu(n) << '\n';
+  }
 
   return 0;
 }
